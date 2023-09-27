0f64b8d5dbb9401f1f5aedb22404124afebf01d7
boriskostadinov96 <boriskostadinov96@gmail.com>
2023-09-27 17:41:15 +0300

    update

---
+++ python_programming_basics/conditional_statements _if_else_elif_debug/l_07_area_of_figures.py
@@ -0,0 +1,23 @@
+from math import pi
+figure = input()
+area = 0
+
+if figure == "square":
+    a = float(input())
+    area = a * a
+
+elif figure == "rectangle":
+    a = float(input())
+    b = float(input())
+    area = a * b
+
+elif figure == "circle":
+    radius = float(input())
+    area = pi * (radius ** 2)
+
+elif figure == "triangle":
+    a = float(input())
+    b = float(input())
+    area = 1 / 2 * a * b
+
+print(area)
