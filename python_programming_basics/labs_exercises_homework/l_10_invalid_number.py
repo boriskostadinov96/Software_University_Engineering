798dcb55e1f438a88f5138ad2748578ec8b82b2f
boriskostadinov96 <boriskostadinov96@gmail.com>
2023-09-27 17:39:11 +0300

    update

--- python_programming_basics/labs_exercises_homework/l_10_invalid_number.py
+++
@@ -1,3 +0,0 @@
-number = int(input())
-if number != 0 and number < 100 or number > 200:
-    print('invalid')
--- python_programming_basics/labs_exercises_homework/p_01_numbers_ending_in_7.py
+++
@@ -1,5 +0,0 @@
-number = int(input())
-
-for number in range (1, 1001):
-    if number % 10 == 7:
-        print(number)
