18aea6bbbc65f3ae21003f9f5ea9671704ae2cf0
boriskostadinov96 <boriskostadinov96@gmail.com>
2023-10-07 19:17:05 +0300

    Create l_03_calculations.py

---
+++ python_programming_fundamentals/functions/l_03_calculations.py
@@ -0,0 +1,23 @@
+def calculate(command_operator, num_one, num_two):
+    result = 0
+    if command_operator == "multiply":
+        result = num_one * num_two
+
+    elif command_operator == "divide":
+        result = num_one // num_two
+
+    elif command_operator == "add":
+        result = num_one + num_two
+
+    elif command_operator == "subtract":
+        result = num_one - num_two
+
+    return result
+
+
+operator_input = input()
+first_number = int(input())
+second_number = int(input())
+
+total_result = calculate(operator_input, first_number, second_number)
+print(total_result)
