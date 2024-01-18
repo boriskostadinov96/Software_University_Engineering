0c9da5bc06a8dd369fe60f919645f236b2080f41
Boris Kostadinov <140704393+boriskostadinov96@users.noreply.github.com>
2024-01-19 00:07:09 +0200

    Update e_04_count_symbols.py

--- python_programming_advanced/tuples_and_sets/e_04_count_symbols.py
+++ python_programming_advanced/tuples_and_sets/e_04_count_symbols.py
@@ -2,3 +2,17 @@ text = input()
 
 for symbol in sorted(set(text)):
     print(f"{symbol}: {text.count(symbol)} time/s")
+
+
+# 1.2
+# text = input()
+# occurrences = {}
+#
+# for chr in text:
+#     if chr not in occurrences:
+#         occurrences[chr] = 1
+#
+#     else:
+#         occurrences[chr] += 1
+#
+# [print(f"{key}: {value} time/s") for key, value in sorted(occurrences.items())]
