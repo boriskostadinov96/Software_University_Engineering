8e8849c7a7a518f8f1ca12aaa0198c2cc2bf4bc8
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-01-28 20:34:32 +0200

    Create e_04_numbers_filter.py

---
+++ python_programming_advanced/functions_advanced/e_04_numbers_filter.py
@@ -0,0 +1,9 @@
+def even_odd_filter(**kwargs):  # {odd: [1, 2, 3, 4, 10, 5], even: [3, 4, 5, 7, 10, 2, 5, 5, 2]}
+
+    if "even" in kwargs:
+        kwargs["even"] = [n for n in kwargs["even"] if n % 2 == 0]
+
+    if "odd" in kwargs:
+        kwargs["odd"] = [n for n in kwargs["odd"] if n % 2 != 0]
+
+    return dict(sorted(kwargs.items(), key=lambda x: -len(x[1])))
