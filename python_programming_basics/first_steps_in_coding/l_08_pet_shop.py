19efe17b0b2ccc5100285f0c692cb04ba7ea4f6a
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-04-25 19:58:24 +0300

    Update l_08_pet_shop.py

--- python_programming_basics/first_steps_in_coding/l_08_pet_shop.py
+++ python_programming_basics/first_steps_in_coding/l_08_pet_shop.py
@@ -1,8 +1,5 @@
-doggs_food = int(input())
-cats_food = int(input())
+dog_food = int(input())
+cat_food = int(input())
 
-dogg_price_food = (doggs_food * 2.5)
-cats_price_food = (cats_food * 4)
-final_price = dogg_price_food + cats_price_food
-
-print(f"{final_price} lv.")
+total_price = dog_food * 2.5 + cat_food * 4
+print(f"{total_price} lv.")
