1f59fce920ecd6a80e2675d9a440670bbb9e6e66
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-04-26 13:47:22 +0300

    Update e_05_supplies_for_school.py

--- python_programming_basics/first_steps_in_coding/e_05_supplies_for_school.py
+++ python_programming_basics/first_steps_in_coding/e_05_supplies_for_school.py
@@ -3,6 +3,7 @@ markers = int(input())
 liquid = int(input())
 discount_percentage = int(input())
 
-total_price = pens * 5.8 + markers * 7.2 + liquid * 1.2
-discount_price = total_price - (total_price * (discount_percentage / 100))
-print(discount_price)
+total_sum = pens * 5.8 + markers * 7.2 + liquid * 1.2
+discount = total_sum * discount_percentage / 100
+
+print(total_sum - discount)
