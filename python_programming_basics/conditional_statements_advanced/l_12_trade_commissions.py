25717db2e49dde51a20f96afff97c745f137c070
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-04-28 15:29:37 +0300

    Update l_12_trade_commissions.py

--- python_programming_basics/conditional_statements_advanced/l_12_trade_commissions.py
+++ python_programming_basics/conditional_statements_advanced/l_12_trade_commissions.py
@@ -1,42 +1,53 @@
-city = input()
-sales_volume = float(input())
-commissions = 0
-
+town = input()
+sales = float(input())
+result = 0
 wrong_data = False
 
-if city == 'Sofia':
-    if 0 <= sales_volume <= 500:
-        commissions = sales_volume * 0.05
-    elif 500 < sales_volume <= 1000:
-        commissions = sales_volume * 0.07
-    elif 1000 < sales_volume <= 10000:
-        commissions = sales_volume * 0.08
-    elif sales_volume > 10000:
-        commissions = sales_volume * 0.12
+if town == "Sofia":
+    if 0 <= sales <= 500:
+        result = sales * 0.05
+
+    elif 500 < sales <= 1000:
+        result = sales * 0.07
+
+    elif 1000 < sales <= 10000:
+        result = sales * 0.08
+
+    elif sales > 10000:
+        result = sales * 0.12
+
     else:
         wrong_data = True
 
-elif city == 'Varna':
-    if 0 <= sales_volume <= 500:
-        commissions = sales_volume * 0.045
-    elif 500 < sales_volume <= 1000:
-        commissions = sales_volume * 0.075
-    elif 1000 < sales_volume <= 10000:
-        commissions = sales_volume * 0.10
-    elif sales_volume > 10000:
-        commissions = sales_volume * 0.13
+elif town == "Varna":
+    if 0 <= sales <= 500:
+        result = sales * 0.045
+
+    elif 500 < sales <= 1000:
+        result = sales * 0.075
+
+    elif 1000 < sales <= 10000:
+        result = sales * 0.10
+
+    elif sales > 10000:
+        result = sales * 0.13
+
     else:
         wrong_data = True
 
-elif city == 'Plovdiv':
-    if 0 <= sales_volume <= 500:
-        commissions = sales_volume * 0.055
-    elif 500 < sales_volume <= 1000:
-        commissions  = sales_volume * 0.08
-    elif 1000 < sales_volume <= 10000:
-        commissions = sales_volume * 0.12
-    elif sales_volume > 10000:
-        commissions = sales_volume * 0.145
+elif town == "Plovdiv":
+    if 0 <= sales <= 500:
+        result = sales * 0.055
+
+    elif 500 < sales <= 1000:
+        result = sales * 0.08
+
+    elif 1000 < sales <= 10000:
+        result = sales * 0.12
+
+    elif sales > 10000:
+        result = sales * 0.145
+
     else:
         wrong_data = True
 
@@ -44,6 +55,7 @@ else:
     wrong_data = True
 
 if wrong_data:
-    print('error')
+    print("error")
+
 else:
-    print (f'{commissions:.2f}')
+    print(f"{result:.2f}")
