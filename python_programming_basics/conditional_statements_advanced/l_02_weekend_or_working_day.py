9141f0dd00c523037901a58162762a9dab0e7f89
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-04-28 13:33:18 +0300

    Update l_02_weekend_or_working_day.py

--- python_programming_basics/conditional_statements_advanced/l_02_weekend_or_working_day.py
+++ python_programming_basics/conditional_statements_advanced/l_02_weekend_or_working_day.py
@@ -1,17 +1,13 @@
-day_of_week = input()
-if day_of_week == 'Monday':
-    print('Working day')
-elif day_of_week == 'Tuesday':
-    print('Working day')
-elif day_of_week == 'Wednesday':
-    print('Working day')
-elif day_of_week == 'Thursday':
-    print('Working day')
-elif day_of_week == 'Friday':
-    print('Working day')
-elif day_of_week == 'Saturday':
-    print('Weekend')
-elif day_of_week == 'Sunday':
-    print('Weekend')
+day = input()
+
+working_days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+weekend_days = ["Saturday", "Sunday"]
+
+if day in working_days:
+    print("Working day")
+
+elif day in weekend_days:
+    print("Weekend")
+
 else:
-    print('Error')
+    print("Error")
