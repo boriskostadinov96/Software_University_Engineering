aa1b2166c2cb172b22ba6530ad7ba05bb599e68f
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-05-01 21:19:55 +0300

    Update l_02_password.py

--- python_programming_basics/while_loops/l_02_password.py
+++ python_programming_basics/while_loops/l_02_password.py
@@ -1,8 +1,21 @@
 username = input()
-user_password = input()
-current_password = input()
+correct_password = input()
+new_password = input()
 
-while user_password != current_password:
-    current_password = input()
+while new_password != correct_password:
+    new_password = input()
 
-print(f'Welcome {username}!')
+print(f"Welcome {username}!")
+
+# solution 2
+# username = input()
+# correct_password = input()
+#
+# while True:
+#     password = input()
+#     if password != correct_password:
+#         continue
+#
+#     else:
+#         print(f"Welcome {username}!")
+#         break
