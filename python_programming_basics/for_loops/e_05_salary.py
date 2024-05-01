66f6051cc6444f28d92276f951a72b79e1e843d8
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-05-01 14:48:29 +0300

    Create e_05_salary.py

---
+++ python_programming_basics/for_loops/e_05_salary.py
@@ -0,0 +1,21 @@
+open_tabs = int(input())
+salary = int(input())
+
+for _ in range(open_tabs):
+    current_tab = input()
+
+    if current_tab == "Facebook":
+        salary -= 150
+
+    elif current_tab == "Instagram":
+        salary -= 100
+
+    elif current_tab == "Reddit":
+        salary -= 50
+
+    if salary <= 0:
+        print("You have lost your salary.")
+        break
+
+else:
+    print(salary)
