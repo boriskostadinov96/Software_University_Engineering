cdd9f615dbf85c89d05ba3fcab9682042ea54591
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-05-03 15:56:47 +0300

    Update l_08_graduation.py

--- python_programming_basics/while_loops/l_08_graduation.py
+++ python_programming_basics/while_loops/l_08_graduation.py
@@ -1,26 +1,25 @@
 student_name = input()
-grades_counter = 0
-years_counter = 0
-failed_counter = 0
+current_class = 1
+bad_grades = 0
+average_grade = 0
+failed = False
 
-while True:
-    annual_grade = float(input())
+while current_class <= 12:
+    year_grade = float(input())
 
-    if annual_grade < 4:
-        failed_counter += 1
-
-        if failed_counter > 1:
-            current_year = years_counter + 1
-            print(f'{student_name} has been excluded at {current_year} grade')
+    if year_grade < 4:
+        bad_grades += 1
+        if bad_grades > 1:
+            failed = True
             break
-
         continue
 
-    else:
-        grades_counter += annual_grade
-        years_counter += 1
+    current_class += 1
+    average_grade += year_grade / 12
 
-    if years_counter == 12:
-        average_grade = grades_counter / 12
-        print(f'{student_name} graduated. Average grade: {average_grade:.2f}')
-        break
+if failed:
+    print(f"{student_name} has been excluded at {current_class} grade")
+
+else:
+    print(f"{student_name} graduated. Average grade: {average_grade:.2f}")
+
