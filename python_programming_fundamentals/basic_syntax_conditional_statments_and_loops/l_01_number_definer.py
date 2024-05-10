4f4e902654443b6faebdd1c0bae4392eab926581
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-05-10 14:40:50 +0300

    Update l_01_number_definer.py

--- python_programming_fundamentals/basic_syntax_conditional_statments_and_loops/l_01_number_definer.py
+++ python_programming_fundamentals/basic_syntax_conditional_statments_and_loops/l_01_number_definer.py
@@ -2,18 +2,45 @@ number = float(input())
 
 if number == 0:
     print("zero")
+
 elif number > 0:
-    if number < 1:
+    if number < 1 and number != 0:
         print("small positive")
-    elif number > 1000000:
+
+    elif number > 1_000_000:
         print("large positive")
+
     else:
         print("positive")
 
-else:
-    if abs(number) < 1:
+elif number < 0:
+    if abs(number) < 1 and abs(number) != 0:
         print("small negative")
-    elif abs(number) > 1000000:
+
+    elif abs(number) > 1_000_000:
         print("large negative")
+
     else:
         print("negative")
+
+# solution 2
+#
+# number = float(input())
+# 
+# if number == 0:
+#     print("zero")
+# elif number > 0:
+#     if number < 1:
+#         print("small positive")
+#     elif number > 1000000:
+#         print("large positive")
+#     else:
+#         print("positive")
+#
+# else:
+#     if abs(number) < 1:
+#         print("small negative")
+#     elif abs(number) > 1000000:
+#         print("large negative")
+#     else:
+#         print("negative")
