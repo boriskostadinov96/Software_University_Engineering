3ba16b551c2d2ea797bdff248f27a0aea9528739
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-05-10 15:39:32 +0300

    Update l_07_patterns.py

--- python_programming_fundamentals/basic_syntax_conditional_statments_and_loops/l_07_patterns.py
+++ python_programming_fundamentals/basic_syntax_conditional_statments_and_loops/l_07_patterns.py
@@ -1,11 +1,15 @@
-number = int(input())
+max_stars_per_count = int(input())
 
-for i in range(1, number + 1):
+for i in range(1, max_stars_per_count + 1):
+
     for j in range(i):
         print("*", end="")
+
     print()
 
-for i in range(number - 1, 0, -1):
+for i in range(max_stars_per_count - 1, 0, - 1):
+
     for j in range(i):
         print("*", end="")
+
     print()
