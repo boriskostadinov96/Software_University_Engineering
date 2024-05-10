351e0d28512c1c3b7e3245bcbb9b3fd2c48f6420
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-05-10 15:25:25 +0300

    commit

--- python_programming_fundamentals/basic_syntax_conditional_statments_and_loops/l_05_number_between_1_and_100.py
+++ python_programming_fundamentals/basic_syntax_conditional_statments_and_loops/l_05_number_between_1_and_100.py
@@ -1,6 +1,8 @@
 number = float(input())
 
-while number < 1 or number > 100:
-    number = float(input())
+while True:
+    if 1 <= number <= 100:
+        print(f"The number {number} is between 1 and 100")
+        break
 
-print(f"The number {number} is between 1 and 100")
+    number = float(input())
