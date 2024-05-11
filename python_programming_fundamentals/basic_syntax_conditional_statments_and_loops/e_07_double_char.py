081b93e655d6a7d3c7b023509f9353208c340914
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-05-11 21:15:34 +0300

    Update e_07_double_char.py

--- python_programming_fundamentals/basic_syntax_conditional_statments_and_loops/e_07_double_char.py
+++ python_programming_fundamentals/basic_syntax_conditional_statments_and_loops/e_07_double_char.py
@@ -1,9 +1,30 @@
-current_string = input()
+command = input()
 
-while current_string != "End":
-    if current_string != "SoftUni":
-        new_string = ""
-        for character in current_string:
-            new_string += character * 2
-        print(new_string)
-    current_string = input()
+while command != 'End':
+    current_string = str(command)
+
+    if current_string == "SoftUni":
+        command = input()
+        continue
+
+    for letter in current_string:
+        print(letter * 2, end="")
+
+    print()
+
+    command = input()
+
+# solution 2
+# current_string = input()
+#
+# while current_string != "End":
+#
+#     if current_string != "SoftUni":
+#         new_string = ""
+#
+#         for character in current_string:
+#             new_string += character * 2
+#
+#         print(new_string)
+#     
+#     current_string = input()
