2ca131d90cc7b3ab90042875635f78f32ed01766
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-05-11 18:12:31 +0300

    add

--- python_programming_fundamentals/basic_syntax_conditional_statments_and_loops/e_03_chat_codes.py
+++ python_programming_fundamentals/basic_syntax_conditional_statments_and_loops/e_03_chat_codes.py
@@ -1,14 +1,16 @@
-number_of_messages = int(input())
-string = ""
+messages = int(input())
 
-for current_number in range(number_of_messages):
-    number = int(input())
-    if number == 88:
-        string = "Hello"
-    elif number == 86:
-        string = "How are you?"
-    elif number < 88:
-        string = "GREAT!"
-    else:
-        string = "Bye."
-    print(f'{string}')
+for _ in range(messages):
+    current_number = int(input())
+
+    if current_number == 88:
+        print("Hello")
+
+    elif current_number == 86:
+        print("How are you?")
+
+    elif current_number != 88 and current_number != 86 and current_number < 88:
+        print("GREAT!")
+
+    elif current_number > 88:
+        print("Bye.")
