fbef066d1578c49799444cf95e8ee0f6636eb0f4
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-05-11 21:43:22 +0300

    Update e_08_how_much_coffee_do_you_need.py

--- python_programming_fundamentals/basic_syntax_conditional_statments_and_loops/e_08_how_much_coffee_do_you_need.py
+++ python_programming_fundamentals/basic_syntax_conditional_statments_and_loops/e_08_how_much_coffee_do_you_need.py
@@ -1,18 +1,51 @@
 command = input()
-coffe_counter = 0
+events_lower = ["coding", "dog", "cat", "movie"]
+events_upper = ["CODING", "DOG", "CAT", "MOVIE"]
+total_coffees = 0
 
 while command != "END":
-    if command.lower() == "coding" or \
-            command.lower() == "dog" or \
-            command.lower() == "cat" or \
-            command.lower() == "movie":
-        if command.islower():
-            coffe_counter += 1
-        else:
-            coffe_counter += 2
+    coffee_counter = 0
+
+    if command in events_lower:
+        coffee_counter += 1
+
+    elif command in events_upper:
+        coffee_counter += 2
+
+    else:
+        command = input()
+        continue
+
+    total_coffees += coffee_counter
 
     command = input()
-if coffe_counter > 5:
+
+if total_coffees > 5:
     print("You need extra sleep")
+
 else:
-    print(coffe_counter)
+    print(total_coffees)
+
+# solution 2
+# command = input()
+# coffe_counter = 0
+#
+# while command != "END":
+#     if command.lower() == "coding" or \
+#             command.lower() == "dog" or \
+#             command.lower() == "cat" or \
+#             command.lower() == "movie":
+#
+#         if command.islower():
+#             coffe_counter += 1
+#
+#         else:
+#             coffe_counter += 2
+#
+#     command = input()
+#
+# if coffe_counter > 5:
+#     print("You need extra sleep")
+#
+# else:
+#     print(coffe_counter)
