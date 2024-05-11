0ed445dafa4653ed458d96c6f52a845beb710765
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-05-11 13:26:49 +0300

    Update e_02_drink_something.py

--- python_programming_fundamentals/basic_syntax_conditional_statments_and_loops/e_02_drink_something.py
+++ python_programming_fundamentals/basic_syntax_conditional_statments_and_loops/e_02_drink_something.py
@@ -3,12 +3,14 @@ drink = ""
 
 if age <= 14:
     drink = "toddy"
+
 elif age <= 18:
     drink = "coke"
+
 elif age <= 21:
     drink = "beer"
+
 else:
     drink = "whisky"
-print(f'drink {drink}')
 
-
+print(f"drink {drink}")
