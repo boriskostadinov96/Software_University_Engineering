b4921dafcb4137b339c448d3d21f648b82093d10
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-05-11 22:08:19 +0300

    Update e_09_sorting_hat.py

--- python_programming_fundamentals/basic_syntax_conditional_statments_and_loops/e_09_sorting_hat.py
+++ python_programming_fundamentals/basic_syntax_conditional_statments_and_loops/e_09_sorting_hat.py
@@ -1,25 +1,56 @@
 name = input()
-place = ""
-Voldemort = False
+voldemort = False
 
 while name != "Welcome!":
+
     if name == "Voldemort":
-        Voldemort = True
+        voldemort = True
         break
 
     if len(name) < 5:
-        place = "Gryffindor."
+        print(f"{name} goes to Gryffindor.")
 
     elif len(name) == 5:
-        place = "Slytherin."
+        print(f"{name} goes to Slytherin.")
 
     elif len(name) == 6:
-        place = "Ravenclaw."
-    else:
-        place = "Hufflepuff."
-    print(f'{name} goes to {place}')
+        print(f"{name} goes to Ravenclaw.")
+
+    elif len(name) > 6:
+        print(f"{name} goes to Hufflepuff.")
+
     name = input()
-if Voldemort:
+
+if voldemort:
     print("You must not speak of that name!")
+
 else:
     print("Welcome to Hogwarts.")
+
+# solution 2
+# name = input()
+# place = ""
+# Voldemort = False
+#
+# while name != "Welcome!":
+#     if name == "Voldemort":
+#         Voldemort = True
+#         break
+#
+#     if len(name) < 5:
+#         place = "Gryffindor."
+#
+#     elif len(name) == 5:
+#         place = "Slytherin."
+#
+#     elif len(name) == 6:
+#         place = "Ravenclaw."
+#     else:
+#         place = "Hufflepuff."
+#     print(f'{name} goes to {place}')
+#     name = input()
+# if Voldemort:
+#     print("You must not speak of that name!")
+# else:
+#     print("Welcome to Hogwarts.")
+
