c71e1ab6bd1b2104d203f383d5f15864bf7100fa
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-05-12 16:14:25 +0300

    Update e_10_mutate_strings.py

--- python_programming_fundamentals/basic_syntax_conditional_statments_and_loops/e_10_mutate_strings.py
+++ python_programming_fundamentals/basic_syntax_conditional_statments_and_loops/e_10_mutate_strings.py
@@ -1,11 +1,13 @@
-first_string = input()
-second_string = input()
-last_printed_string = first_string
-for character_index in range(len(first_string)):
-    left_part = second_string[:character_index + 1]
-    right_part = first_string[character_index + 1:]
-    new_string = left_part + right_part
-    if new_string == last_printed_string:
-        continue
-    print(new_string)
-    last_printed_string = new_string
+first_str = input()
+second_str = input()
+repeated_str = first_str
+
+for char_idx in range(len(first_str)):
+    left_part = second_str[:char_idx + 1]
+    right_part = first_str[char_idx + 1:]
+
+    new_str = left_part + right_part
+
+    if new_str != repeated_str:
+        print(new_str)
+        repeated_str = new_str
