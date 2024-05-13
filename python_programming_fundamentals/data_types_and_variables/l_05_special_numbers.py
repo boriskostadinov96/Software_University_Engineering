208981ceb4e76809940174ea3466b78b08c2ee13
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-05-13 21:47:43 +0300

    Update l_05_special_numbers.py

--- python_programming_fundamentals/data_types_and_variables/l_05_special_numbers.py
+++ python_programming_fundamentals/data_types_and_variables/l_05_special_numbers.py
@@ -1,11 +1,9 @@
 number = int(input())
-for current_number in range(1, number + 1):
-    current_number_as_string = str(current_number)
-    digits_sum = 0
-    for digit in current_number_as_string:
-        digits_sum += int(digit)
-    is_special = False
-    if digits_sum == 5 or digits_sum == 7 or digits_sum == 11:
-        is_special = True
-    print(f"{current_number} -> {is_special}")
 
+for current_number in range(1, number + 1):
+    digits_sum = sum(int(digit) for digit in str(current_number))
+
+    if digits_sum == 5 or digits_sum == 7 or digits_sum == 11:
+        print(f"{current_number} -> True")
+    else:
+        print(f"{current_number} -> False")
