19d1a6ea8a1eef0c774fc10ed95d7e5554712c02
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-05-13 17:16:45 +0300

    Update l_02_convert_meters_to_kilometers.py

--- python_programming_fundamentals/data_types_and_variables/l_02_convert_meters_to_kilometers.py
+++ python_programming_fundamentals/data_types_and_variables/l_02_convert_meters_to_kilometers.py
@@ -1,3 +1,8 @@
-distance_in_meters = int(input())
-kilometers = distance_in_meters / 1000
-print(f'{kilometers:.2f}')
+meters = int(input())
+kilometers = meters * 0.001
+print(f"{kilometers:.2f}")
+
+# solution 2
+# distance_in_meters = int(input())
+# kilometers = distance_in_meters / 1000
+# print(f'{kilometers:.2f}')
