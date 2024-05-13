8c7ed1de3cc965d5c125eeb8f12803e34bf1e57e
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-05-13 21:14:12 +0300

    Update l_06_next_happy_year.py

--- python_programming_fundamentals/data_types_and_variables/l_06_next_happy_year.py
+++ python_programming_fundamentals/data_types_and_variables/l_06_next_happy_year.py
@@ -1,11 +1,25 @@
-year = int(input())
-year_is_special = False
-while not year_is_special:
-    year += 1
-    year_as_string = str(year)
-    year_is_special = True
-    for digit in year_as_string:
-        if year_as_string.count(digit) > 1:
-            year_is_special = False
-            break
-print(year)
+current_year = int(input())
+
+while True:
+    current_year += 1
+
+    if len(set(str(current_year))) == len(str(current_year)):
+        print(current_year)
+        break
+
+# solution 2
+# year = int(input())
+# year_is_special = False
+#
+# while not year_is_special:
+#     year += 1
+#     year_as_string = str(year)
+#     year_is_special = True
+#
+#     for digit in year_as_string:
+#
+#         if year_as_string.count(digit) > 1:
+#             year_is_special = False
+#             break
+#
+# print(year)
