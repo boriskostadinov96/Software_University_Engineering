a32ea9b12c978b011f29c34ff639cece93c994a1
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-05-13 14:15:26 +0300

    Update e_11_easter_bread.py

--- python_programming_fundamentals/basic_syntax_conditional_statments_and_loops/e_11_easter_bread.py
+++ python_programming_fundamentals/basic_syntax_conditional_statments_and_loops/e_11_easter_bread.py
@@ -1,15 +1,22 @@
 budget = float(input())
-one_kg_flour_price = float(input())
-breads_counter = 0
-colored_eggs_counter = 0
-eggs_price = one_kg_flour_price * 0.75
-milk_price = one_kg_flour_price * 1.25 / 4
-one_bread_price = eggs_price + one_kg_flour_price + milk_price
-while budget >= one_bread_price:
-    budget -= one_bread_price
-    breads_counter += 1
-    colored_eggs_counter += 3
-    if breads_counter % 3 == 0:
-        colored_eggs_counter -= breads_counter - 2
-print(f"You made {breads_counter} loaves of Easter bread! Now you have {colored_eggs_counter} eggs and {budget:.2f}BGN left.")
+flour_price = float(input())
+number_of_loaves = 0
+colored_eggs = 0
+
+eggs_price = flour_price * 0.75
+milk_price = flour_price * 1.25 / 4
+
+loaves = flour_price + eggs_price + milk_price
+
+while budget >= loaves:
+
+    number_of_loaves += 1
+    colored_eggs += 3
+
+    if number_of_loaves % 3 == 0:
+        colored_eggs -= number_of_loaves - 2
+
+    budget -= loaves
+
+print(f"You made {number_of_loaves} loaves of Easter bread! Now you have {colored_eggs} eggs and {budget:.2f}BGN left.")
 
