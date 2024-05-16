44b5dff1b3e4c6dc146110a16e6657e501400eb0
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-05-16 16:45:56 +0300

    Create l_02_insert_data_in_tables.sql

---
+++ PostgreSQL/01_postgresql_introduction_data_types_working_with_tables/l_02_insert_data_in_tables.sql
@@ -0,0 +1,8 @@
+INSERT into employees (first_name, last_name, hiring_date, salary, devices_number)
+
+VALUES
+	('Koko', 'Bokov', '2012-02-03', 1500.00, 123),
+	('Doko', 'Vokov', '2013-05-04', 1800.00, 124),
+	('Loko', 'Zokov', '2014-05-01', 1900.00, 125);
+
+SELECT * FROM employees
