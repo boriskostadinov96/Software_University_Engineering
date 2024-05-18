611db512e3facfb08ec29d514219dad620eed9b3
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-05-18 22:21:19 +0300

    Update e_09_snowballs.py

--- python_programming_fundamentals/data_types_and_variables/e_09_snowballs.py
+++ python_programming_fundamentals/data_types_and_variables/e_09_snowballs.py
@@ -1,16 +1,17 @@
-number_of_snowballs = int(input())
-max_weight = 0
-max_time = 0
-max_quality = 0
-max_value = 0
-for current_snowball in range(number_of_snowballs):
-    current_weight = int(input())
-    current_time = int(input())
-    current_quality = int(input())
-    current_value = (current_weight / current_time) ** current_quality
-    if current_value > max_value:
-        max_weight = current_weight
-        max_time = current_time
-        max_quality = current_quality
-        max_value = current_value
-print(f"{max_weight} : {max_time} = {int(max_value)} ({max_quality})")
+snowballs = int(input())
+highest_value = 0
+best_snowball = (0, 0, 0)
+
+for _ in range(snowballs):
+    weight = int(input())
+    time_needed = int(input())
+    quality = int(input())
+
+    value = (weight / time_needed) ** quality
+
+    if value > highest_value:
+        highest_value = value
+        best_snowball = (weight, time_needed, quality)
+
+weight, time_needed, quality = best_snowball
+print(f"{weight} : {time_needed} = {int(highest_value)} ({quality})")
