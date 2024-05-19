2f7c31d67bfc42762d70f2b1692ecbc5476ead38
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-05-19 14:22:36 +0300

    Update e_07_water_overflow.py

--- python_programming_fundamentals/data_types_and_variables/e_07_water_overflow.py
+++ python_programming_fundamentals/data_types_and_variables/e_07_water_overflow.py
@@ -1,13 +1,33 @@
-number_of_lines = int(input())
-water_counter = 0
 tank_capacity = 255
+lines = int(input())
+water_to_pour = 0
 
-for line in range(number_of_lines):
-    liter_of_water = int(input())
-    if tank_capacity - liter_of_water < 0:
-        print("Insufficient capacity!" )
+for _ in range(lines):
+    liters_of_water = int(input())
+
+    if tank_capacity >= liters_of_water:
+        tank_capacity -= liters_of_water
+
+        water_to_pour += liters_of_water
+
+    else:
+        print("Insufficient capacity!")
         continue
 
-    tank_capacity -= liter_of_water
-    water_counter += liter_of_water
-print(water_counter)
+print(water_to_pour)
+
+
+# solution 2
+# number_of_lines = int(input())
+# water_counter = 0
+# tank_capacity = 255
+#
+# for line in range(number_of_lines):
+#     liter_of_water = int(input())
+#     if tank_capacity - liter_of_water < 0:
+#         print("Insufficient capacity!" )
+#         continue
+#
+#     tank_capacity -= liter_of_water
+#     water_counter += liter_of_water
+# print(water_counter)
