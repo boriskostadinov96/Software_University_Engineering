fc72d2c146b0b5df8277a5aad0c2c7d6695f4888
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-05-26 21:13:10 +0300

    Update e_02_multiples_list.py

--- python_programming_fundamentals/lists_basic/e_02_multiples_list.py
+++ python_programming_fundamentals/lists_basic/e_02_multiples_list.py
@@ -1,6 +1,15 @@
 factor = int(input())
 count = int(input())
-list_with_numbers = []
-for multiplier in range(1, count + 1):
-    list_with_numbers.append(factor * multiplier)
-print(list_with_numbers)
+
+numbers = [factor * n for n in range(1, count + 1)]
+print(numbers)
+
+# solution 2
+# factor = int(input())
+# count = int(input())
+# list_with_numbers = []
+#
+# for multiplier in range(1, count + 1):
+#     list_with_numbers.append(factor * multiplier)
+#
+# print(list_with_numbers)
