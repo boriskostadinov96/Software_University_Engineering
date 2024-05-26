1019db0d950af7812b8de7d73f18df259fbe8b8f
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-05-26 20:48:17 +0300

    Update e_01_invert_values.py

--- python_programming_fundamentals/lists_basic/e_01_invert_values.py
+++ python_programming_fundamentals/lists_basic/e_01_invert_values.py
@@ -1,6 +1,13 @@
-list_with_numbers = input().split()
-opposite_numbers = []
-for number in list_with_numbers:
-    current_number = -int(number)
-    opposite_numbers.append(current_number)
-print(opposite_numbers)
+numbers = input().split()
+opposite_list = [-int(n) for n in numbers]
+print(opposite_list)
+
+# solution 2
+# list_with_numbers = input().split()
+# opposite_numbers = []
+#
+# for number in list_with_numbers:
+#     current_number = -int(number)
+#     opposite_numbers.append(current_number)
+#
+# print(opposite_numbers)
