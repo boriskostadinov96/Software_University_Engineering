43062d701f6ab380c76994ef2becb8b3426d0a80
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-05-26 15:43:11 +0300

    Update l_03_list_statistics.py

--- python_programming_fundamentals/lists_basic/l_03_list_statistics.py
+++ python_programming_fundamentals/lists_basic/l_03_list_statistics.py
@@ -1,14 +1,17 @@
-number = int(input())
+lines = int(input())
 positive_numbers = []
 negative_numbers = []
 
-for _ in range(number):
-    current_numbers = int(input())
-    if current_numbers >= 0:
-        positive_numbers.append(current_numbers)
+for _ in range(lines):
+    current_number = int(input())
+
+    if current_number < 0:
+        negative_numbers.append(current_number)
+
     else:
-        negative_numbers.append(current_numbers)
+        positive_numbers.append(current_number)
+
 print(positive_numbers)
 print(negative_numbers)
-print(f'Count of positives: {len(positive_numbers)}')
-print(f'Sum of negatives: {sum(negative_numbers)}')
+print(f"Count of positives: {len(positive_numbers)}")
+print(f"Sum of negatives: {sum(negative_numbers)}")
