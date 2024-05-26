b2d69ea2d19062b419e0ac4e2baa0984a1061bc3
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-05-26 23:21:42 +0300

    Update e_06_survival_of_the_biggest.py

--- python_programming_fundamentals/lists_basic/e_06_survival_of_the_biggest.py
+++ python_programming_fundamentals/lists_basic/e_06_survival_of_the_biggest.py
@@ -1,12 +1,22 @@
-string = input()
-counter = int(input())
+integers = [int(x) for x in input().split()]
+numbers_to_remove = int(input())
 
-list_of_numbers = string.split(' ')
-list_of_numbers = list(map(lambda x: int(x), list_of_numbers))
+for _ in range(numbers_to_remove):
+    smallest_num = min(integers)
+    integers.remove(smallest_num)
 
-for i in range(counter):
-    smallest = min(list_of_numbers)
-    list_of_numbers.remove(smallest)
+print(*integers, sep=", ")
 
-list_of_numbers = list(map(lambda x: str(x), list_of_numbers))
-print(", ".join(list_of_numbers))
+# solution 2
+# string = input()
+# counter = int(input())
+#
+# list_of_numbers = string.split(' ')
+# list_of_numbers = list(map(lambda x: int(x), list_of_numbers))
+#
+# for i in range(counter):
+#     smallest = min(list_of_numbers)
+#     list_of_numbers.remove(smallest)
+#
+# list_of_numbers = list(map(lambda x: str(x), list_of_numbers))
+# print(", ".join(list_of_numbers))
