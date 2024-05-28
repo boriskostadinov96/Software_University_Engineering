54ecdadf515aceaad7e07b98d3f46ac0a69ac608
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-05-28 14:17:50 +0300

    Update e_01_river_info.sql

--- PostgreSQL/03_built_in_functions/e_01_river_info.sql
+++ PostgreSQL/03_built_in_functions/e_01_river_info.sql
@@ -1,15 +1,18 @@
-CREATE VIEW view_river_info AS
+CREATE OR REPLACE VIEW
+	view_river_info
+AS
 SELECT
-	CONCAT(
-		'The river ', 
-		river_name, 
-		' flows into the ', 
-		outflow, 
-		' and is ', 
-		length, 
-		' kilometers long.'
+	CONCAT_WS(
+		' ',
+		'The river',
+		river_name,
+		'flows into the',
+		outflow,
+		'and is', 
+		"length", 
+		'kilometers long.'
 	) AS "River Information"
 FROM
 	rivers
 ORDER BY
-	river_name ASC;
+	river_name;
