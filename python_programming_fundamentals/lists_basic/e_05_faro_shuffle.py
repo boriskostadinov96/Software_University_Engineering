867e586307e8e2f702b6995bc56f323c91f5c06f
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-05-28 11:56:22 +0300

    Update e_05_faro_shuffle.py

--- python_programming_fundamentals/lists_basic/e_05_faro_shuffle.py
+++ python_programming_fundamentals/lists_basic/e_05_faro_shuffle.py
@@ -1,12 +1,17 @@
 deck_of_cards = input().split()
-count_of_shuffles = int(input())
-for shuffle in range(count_of_shuffles):
-    middle_of_deck = len(deck_of_cards) // 2
-    left_part = deck_of_cards[0:middle_of_deck]
-    right_part = deck_of_cards[middle_of_deck:]
+shuffles_count = int(input())
+
+for _ in range(shuffles_count):
+    middle_part = len(deck_of_cards) // 2
+
+    left_part = deck_of_cards[:middle_part]
+    right_part = deck_of_cards[middle_part:]
     deck_after_shuffling = []
-    for card_index in range(len(left_part)): #(len(right_part))
+
+    for card_index in range(len(left_part)):
         deck_after_shuffling.append(left_part[card_index])
         deck_after_shuffling.append(right_part[card_index])
+
     deck_of_cards = deck_after_shuffling
-print(deck_after_shuffling)
+
+print(deck_of_cards)
