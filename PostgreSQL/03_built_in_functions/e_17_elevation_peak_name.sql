7d2fba357a413a17412fec8c36985449eb2b4542
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-05-29 18:54:23 +0300

    Create e_17_elevation_peak_name.sql

---
+++ PostgreSQL/03_built_in_functions/e_17_elevation_peak_name.sql
@@ -0,0 +1,21 @@
+SELECT
+	CONCAT(
+	elevation,
+	' --->> ',
+	peak_name) AS "Elevation --->> Peak Name"
+FROM
+	peaks
+WHERE elevation >= 4884
+
+-- solution 2
+-- SELECT
+-- 	CONCAT_WS(
+-- 		' ',
+-- 		elevation,
+-- 		REPEAT('-', 3) || REPEAT('>', 2),
+-- 		peak_name
+-- 	) AS "Elevation -->> Peak Name"
+-- FROM
+-- 	peaks
+-- WHERE
+-- 	elevation >= 4884;
