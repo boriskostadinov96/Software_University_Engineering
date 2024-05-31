58b315078fa180753acd15b832a4684493454f64
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-05-31 11:27:40 +0300

    Create l_08_department_names.sql

---
+++ PostgreSQL/04_data_aggregation/l_08_department_names.sql
@@ -0,0 +1,33 @@
+SELECT
+    id,
+    first_name,
+    last_name,
+    salary,
+    department_id,
+    
+    CASE department_id
+        WHEN 1 THEN 'Management'
+        WHEN 2 THEN 'Kitchen Staff'
+        WHEN 3 THEN 'Service Staff'
+        ELSE 'Other'
+    END AS department_name
+FROM 
+    employees;
+
+
+-- solution 2
+-- SELECT
+--     id,
+--     first_name,
+--     last_name,
+--     salary,
+--     department_id,
+    
+--     CASE
+--         WHEN department_id = 1 THEN 'Management'
+--         WHEN department_id = 2 THEN 'Kitchen Staff'
+--         WHEN department_id = 3 THEN 'Service Staff'
+--         ELSE 'Other'
+--     END AS department_name
+-- FROM 
+--     employees;
