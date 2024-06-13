62776e44975d8f4db51923b328a95b23160736d8
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-06-13 11:34:55 +0300

    Update e_09_hello_france.py

--- python_programming_fundamentals/lists_basic/e_09_hello_france.py
+++ python_programming_fundamentals/lists_basic/e_09_hello_france.py
@@ -1,48 +1,98 @@
-train_ticket = 150
-collection = input().split("|")
+items = input().split("|")
 budget = float(input())
-new_budget = 0
-budget_left = budget
-spend = 0
+sell_prices = []
+profit = 0
+train_ticket = 150
 
-clothes_maximum = 50.00
-shoes_maximum = 35.00
-accessories_maximum = 20.50
+for item in items:
+    type, buying_price = item.split("->")
+    buying_price = float(buying_price)
+    price_is_valid = False
 
-list_of_new_prices = []
+    if type == "Clothes":
+        if buying_price <= 50.00:
+            price_is_valid = True
 
-for item in collection:
-    item_type, item_price = item.split('->')
-    item_price = float(item_price)
+    elif type == "Shoes":
+        if buying_price <= 35.00:
+            price_is_valid = True
 
-    if item_type == "Clothes":
-        if item_price <= clothes_maximum and budget_left >= item_price:
-            budget_left -= item_price
-            new_price = item_price * 1.40
-            spend += item_price
-            new_budget += new_price
-            list_of_new_prices.append("{:.2f}".format(new_price))
-    elif item_type == "Shoes":
-        if item_price <= shoes_maximum and budget_left >= item_price:
-            budget_left -= item_price
-            new_price = item_price * 1.40
-            spend += item_price
-            new_budget += new_price
-            list_of_new_prices.append("{:.2f}".format(new_price))
-    elif item_type == "Accessories":
-        if item_price <= accessories_maximum and budget_left >= item_price:
-            budget_left -= item_price
-            new_price = item_price * 1.40
-            spend += item_price
-            new_budget += new_price
-            list_of_new_prices.append("{:.2f}".format(new_price))
+    elif type == "Accessories":
+        if buying_price <= 20.50:
+            price_is_valid = True
 
-profit = new_budget - spend
-total_money = new_budget + budget_left
+    if price_is_valid:
+        if budget >= buying_price:
+            budget -= buying_price
+            sell_price = buying_price * 1.40
+            profit += sell_price - buying_price
+            sell_prices.append(sell_price)
+# Option 1
+for sell_price in sell_prices:
+    print(f"{sell_price:.2f}", end=" ")
+print()
 
-print(*list_of_new_prices, sep=" ")
 print(f"Profit: {profit:.2f}")
-if total_money >= train_ticket:
+total_income = budget + sum(sell_prices)
+
+if total_income >= train_ticket:
     print("Hello, France!")
 else:
     print("Not enough money.")
+
+# Option 2
+# print(" ".join([f"{sell_price:.2f}" for sell_price in sell_prices]))
+
+# Option 3
+# sell_prices_as_string = []
+# for sell_price in sell_prices:
+#     sell_prices_as_string.append(f"{sell_price:.2f}")
+# print(" ".join(sell_prices_as_string))
+
+# solution 2
+# collection = input().split('|')
+# budget = float(input())
+# bought_items = []
+# train_ticket = 150
+# clothes_max_price = 50.00
+# shoes_max_price = 35.00
+# accessories_max_price = 20.50
+#
+# for both_parts in collection:
+#     item_name, item_price = both_parts.split('->')
+#     item_price = float(item_price)
+#
+#     if budget == 0:
+#         break
+#
+#     if budget < item_price:
+#         continue
+#
+#     if item_name == 'Clothes' and item_price <= clothes_max_price:
+#         budget -= item_price
+#         bought_items.append(item_price * 1.4)
+#
+#     elif item_name == 'Shoes' and item_price <= shoes_max_price:
+#         budget -= item_price
+#         bought_items.append(item_price * 1.4)
+#
+#     elif item_name == 'Accessories' and item_price <= accessories_max_price:
+#         budget -= item_price
+#         bought_items.append(item_price * 1.4)
+#
+# initial_total = sum([price / 1.4 for price in bought_items])
+# final_total = sum(bought_items)
+# profit = final_total - initial_total
+#
+#
+# budget += final_total
+#
+#
+# final_prices = ' '.join([f'{x:.2f}' for x in bought_items])
+# print(final_prices)
+# print(f"Profit: {profit:.2f}")
+#
+# if budget >= train_ticket:
+#     print("Hello, France!")
+# else:
+#     print("Not enough money.")
