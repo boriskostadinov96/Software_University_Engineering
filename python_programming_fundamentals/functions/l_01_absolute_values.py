9c71827a8e9f148c03d4e5ab5120978a787ebc9b
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-06-15 22:04:36 +0300

    Update l_01_absolute_values.py

--- python_programming_fundamentals/functions/l_01_absolute_values.py
+++ python_programming_fundamentals/functions/l_01_absolute_values.py
@@ -1,9 +1,2 @@
-numbers_as_string = input().split()
-list_of_numbers = []
-
-for x in numbers_as_string:
-    list_of_numbers.append(abs(float(x)))
-
-print (list_of_numbers)
-
-
+numbers = [abs(float(x)) for x in input().split()]
+print(numbers)
