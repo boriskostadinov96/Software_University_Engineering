e7cc2feaa0dfe168279f663d15cb796b9c95e9eb
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-06-15 21:16:34 +0300

    Update me_05_tic_tac_toe.py

--- python_programming_fundamentals/lists_basic/me_05_tic_tac_toe.py
+++ python_programming_fundamentals/lists_basic/me_05_tic_tac_toe.py
@@ -12,7 +12,6 @@ def check_for_win(digit):
         exit()
 
 
-# checking horizontal lines
 if first_line[0] == first_line[1] and first_line[1] == first_line[2]:
     check_for_win(first_line[0])
 elif second_line[0] == second_line[1] and second_line[1] == second_line[2]:
@@ -20,7 +19,6 @@ elif second_line[0] == second_line[1] and second_line[1] == second_line[2]:
 elif third_line[0] == third_line[1] and third_line[1] == third_line[2]:
     check_for_win(third_line[0])
 
-# checking vertical lines
 if first_line[0] == second_line[0] and second_line[0] == third_line[0]:
     check_for_win(first_line[0])
 elif first_line[1] == second_line[1] and second_line[1] == third_line[1]:
@@ -28,7 +26,6 @@ elif first_line[1] == second_line[1] and second_line[1] == third_line[1]:
 elif first_line[2] == second_line[2] and second_line[2] == third_line[2]:
     check_for_win(first_line[2])
 
-# checking diagonal lines
 if first_line[0] == second_line[1] and second_line[1] == third_line[2]:
     check_for_win(first_line[0])
 elif first_line[2] == second_line[1] and second_line[1] == third_line[0]:
