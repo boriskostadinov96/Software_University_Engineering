19a410442bc7f7caa565dd55db5545fcd844fcfa
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-06-15 10:31:23 +0300

    Create fe_06_top_three_most_commented_photos.sql

---
+++ PostgreSQL/09_final_exam/fe_06_top_three_most_commented_photos.sql
@@ -0,0 +1,17 @@
+SELECT
+    p.id AS photo_id,
+    p.capture_date,
+    p.description,
+    COUNT(c.id) AS comments_count
+FROM
+    photos AS p
+JOIN
+    comments AS c ON c.photo_id = p.id
+WHERE
+    p.description IS NOT NULL AND p.description <> ''
+GROUP BY
+    p.id, p.capture_date, p.description
+ORDER BY
+    comments_count DESC,
+    photo_id ASC
+LIMIT 3;
