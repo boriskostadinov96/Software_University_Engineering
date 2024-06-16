de782bc6fa985fe069a956724cb231ea0b9c55fc
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-06-16 16:26:02 +0300

    Update l_03_calculations.py

--- python_programming_fundamentals/functions/l_03_calculations.py
+++ python_programming_fundamentals/functions/l_03_calculations.py
@@ -1,23 +1,45 @@
-def calculate(command_operator, num_one, num_two):
-    result = 0
-    if command_operator == "multiply":
-        result = num_one * num_two
+def calculations(operator, first_num, second_num):
+    if operator_symbol == 'subtract':
+        return a - b
 
-    elif command_operator == "divide":
-        result = num_one // num_two
+    elif operator == 'add':
+        return a + b
 
-    elif command_operator == "add":
-        result = num_one + num_two
+    elif operator == 'multiply':
+        return a * b
 
-    elif command_operator == "subtract":
-        result = num_one - num_two
-
-    return result
+    else:
+        return a // b
 
 
-operator_input = input()
-first_number = int(input())
-second_number = int(input())
+operator_symbol = input()
+a = int(input())
+b = int(input())
 
-total_result = calculate(operator_input, first_number, second_number)
-print(total_result)
+print(calculations(operator_symbol, a, b))
+
+# solution 2 
+#
+# def calculate(command_operator, num_one, num_two):
+#     result = 0
+#     if command_operator == "multiply":
+#         result = num_one * num_two
+#
+#     elif command_operator == "divide":
+#         result = num_one // num_two
+#
+#     elif command_operator == "add":
+#         result = num_one + num_two
+#
+#     elif command_operator == "subtract":
+#         result = num_one - num_two
+#
+#     return result
+#
+#
+# operator_input = input()
+# first_number = int(input())
+# second_number = int(input())
+#
+# total_result = calculate(operator_input, first_number, second_number)
+# print(total_result)
