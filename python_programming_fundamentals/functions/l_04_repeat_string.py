2f92fcc2da965ebe33c5314f58953618a09d1137
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-06-16 16:47:35 +0300

    Update l_04_repeat_string.py

--- python_programming_fundamentals/functions/l_04_repeat_string.py
+++ python_programming_fundamentals/functions/l_04_repeat_string.py
@@ -1,7 +1,18 @@
-string = input()
-n = int(input())
+def repeat_string(some_str, n):
+    return some_str * n
 
-repeat_string = lambda a, b: a * b
 
-result = repeat_string(string, n)
-print(result)
+text = input()
+counter = int(input())
+
+print(repeat_string(text, counter))
+
+# solution 2 using lambda function
+#
+# string = input()
+# n = int(input())
+# 
+# repeat_string = lambda a, b: a * b
+#
+# result = repeat_string(string, n)
+# print(result)
