1f8de6b7c5045054909997f2757ea8559786b017
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-06-16 16:58:43 +0300

    Update l_05_orders.py

--- python_programming_fundamentals/functions/l_05_orders.py
+++ python_programming_fundamentals/functions/l_05_orders.py
@@ -1,24 +1,37 @@
-def calculate_total_amount(product, quantity):
+def calculate_orders(product_name, product_quantity):
     result = 0
-    if product == "coffee":
-        result = quantity * 1.50
 
-    elif product == "water":
-        result = quantity * 1.00
+    if product_name == 'water':
+        result = product_quantity * 1.00
 
-    elif product == "coke":
-        result = quantity * 1.40
+    elif product_name == 'coffee':
+        result = product_quantity * 1.50
 
-    elif product == "snacks":
-        result = quantity * 2.00
+    elif product_name == 'coke':
+        result = product_quantity * 1.40
+
+    elif product_name == 'snacks':
+        result = product_quantity * 2.00
 
     return result
 
 
-product_name = input()
-requested_quantity = int(input())
+product = input()
+quantity = int(input())
 
-total_amount = calculate_total_amount(product_name, requested_quantity)
-print(f'{total_amount:.2f}')
+print(f"{calculate_orders(product, quantity):.2f}")
 
-
+# solution 2
+# def orders_price(product_name: str, quantity_products: int):
+#     shop_price = {"coffee": 1.50, "water": 1.00, "coke": 1.40, "snacks": 2.00}
+#
+#     if product_name in shop_price:
+#         price_per_item = shop_price[product_name]
+#         total_price = price_per_item * quantity_products
+#
+#         return f"{total_price:.2f}"
+#
+#
+# product = input()
+# quantity = int(input())
+# print(orders_price(product, quantity))
