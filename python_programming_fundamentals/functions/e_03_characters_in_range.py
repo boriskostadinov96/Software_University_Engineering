3d9f3fa7665540665d3fcbbe27bf9054bb3a0f76
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-06-17 01:21:38 +0300

    Update e_03_characters_in_range.py

--- python_programming_fundamentals/functions/e_03_characters_in_range.py
+++ python_programming_fundamentals/functions/e_03_characters_in_range.py
@@ -1,11 +1,27 @@
-def all_the_characters(first_char: str, second_char: str) -> list:
-    characters = []
-    for current_character_as_digit in range(ord(first_char) + 1, ord(second_char)):
-        characters.append(chr(current_character_as_digit))
-    return characters
+def find_string(first_letter, second_letter):
+    result = ''
+
+    for current_char in range(ord(first_letter) + 1, ord(second_letter), + 1):
+        result += chr(current_char) + ' '
+
+    print(result)
 
 
-first_character = input()
-second_character = input()
-final_result = all_the_characters(first_character, second_character)
-print(" ".join(final_result))
+first_char = input()
+second_char = input()
+
+find_string(first_char, second_char)
+
+# solution 2
+#
+# def all_the_characters(first_char: str, second_char: str) -> list:
+#     characters = []
+#     for current_character_as_digit in range(ord(first_char) + 1, ord(second_char)):
+#         characters.append(chr(current_character_as_digit))
+#     return characters
+#
+#
+# first_character = input()
+# second_character = input()
+# final_result = all_the_characters(first_character, second_character)
+# print(" ".join(final_result))
