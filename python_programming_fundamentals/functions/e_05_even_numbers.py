850b8908297f6f356ca0f3d8308c0c71fc5d88de
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-06-17 14:21:05 +0300

    Update e_05_even_numbers.py

--- python_programming_fundamentals/functions/e_05_even_numbers.py
+++ python_programming_fundamentals/functions/e_05_even_numbers.py
@@ -1,7 +1,24 @@
-numbers_as_string = input().split()
-numbers_as_digits = []
-for number in numbers_as_string:
-    numbers_as_digits.append(int(number))
-is_even = lambda x: x % 2 == 0
-result = list(filter(is_even, numbers_as_digits))
-print(result)
+def find_even_numbers(numbers):
+    even_numbers = []
+
+    for x in numbers:
+        if x % 2 == 0:
+            even_numbers.append(x)
+
+    return even_numbers
+
+
+nums = [int(x) for x in input().split()]
+print(find_even_numbers(nums))
+
+# solution 2
+# numbers_as_string = input().split()
+# numbers_as_digits = []
+#
+# for number in numbers_as_string:
+#     numbers_as_digits.append(int(number))
+#
+# is_even = lambda x: x % 2 == 0
+#
+# result = list(filter(is_even, numbers_as_digits))
+# print(result)
