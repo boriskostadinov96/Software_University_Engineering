233ad71f3dc20c3227b285aadb65aa13e2f9a970
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-06-17 14:50:49 +0300

    Update e_08_palindrome_integers.py

--- python_programming_fundamentals/functions/e_08_palindrome_integers.py
+++ python_programming_fundamentals/functions/e_08_palindrome_integers.py
@@ -1,7 +1,22 @@
-def is_palindrome(some_number_as_string: str) -> bool:
-    return some_number_as_string == some_number_as_string[::-1]
+def check_is_number_a_palindrome(integers):
+    for number in integers:
+        reversed_num = number[::-1]
+
+        if number == reversed_num:
+            print('True')
+
+        else:
+            print('False')
 
 
-numbers_as_string = input().split(", ")
-for number_as_string in numbers_as_string:
-    print(is_palindrome(number_as_string))
+nums = input().split(', ')
+print(check_is_number_a_palindrome(nums))
+
+# solution 2
+# def is_palindrome(some_number_as_string: str) -> bool:
+#     return some_number_as_string == some_number_as_string[::-1]
+#
+#
+# numbers_as_string = input().split(", ")
+# for number_as_string in numbers_as_string:
+#     print(is_palindrome(number_as_string))
