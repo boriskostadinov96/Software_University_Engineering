0519f89909a87e62ce9fad5530eb539c06a0afe8
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-06-24 19:28:14 +0300

    Update e_10_perfect_number.py

--- python_programming_fundamentals/functions/e_10_perfect_number.py
+++ python_programming_fundamentals/functions/e_10_perfect_number.py
@@ -1,12 +1,15 @@
-def is_perfect(some_number: int) -> str:
+def is_number_perfect(num):
     divisors_sum = 0
-    for divisor in range(1, some_number):
-        if some_number % divisor == 0:
+
+    for divisor in range(1, num):
+        if num % divisor == 0:
             divisors_sum += divisor
-    if some_number == divisors_sum:
+
+    if num == divisors_sum:
         return "We have a perfect number!"
+
     return "It's not so perfect."
 
 
 number = int(input())
-print(is_perfect(number))
+print(is_number_perfect(number))
