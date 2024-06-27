b1b064ff932ff60f4fd515d257dcc418062acf07
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-06-27 10:59:41 +0300

    Update me_05_multiplication_sign.py

--- python_programming_fundamentals/functions/me_05_multiplication_sign.py
+++ python_programming_fundamentals/functions/me_05_multiplication_sign.py
@@ -1,15 +1,47 @@
-def multiplication(num_one, num_two, num_three):
-    if num_one == 0 or num_two == 0 or num_three == 0:
-        return "zero"
-
-    elif num_one * num_two * num_three > 0:
-        return "positive"
+def check_result(first_num, second_num, third_num):
+    if first_num == 0 or second_num == 0 or third_num == 0:
+        print('zero')
 
     else:
-        return "negative"
+        negative_count = 0
+
+        if first_num < 0:
+            negative_count += 1
+
+        if second_num < 0:
+            negative_count += 1
+
+        if third_num < 0:
+            negative_count += 1
+
+        if negative_count % 2 == 1:
+            print('negative')
+        else:
+            print('positive')
 
 
 first_number = int(input())
 second_number = int(input())
 third_number = int(input())
-print(multiplication(first_number, second_number, third_number))
+
+check_result(first_number, second_number, third_number)
+
+
+# def check_result(first_num, second_num, third_num):
+#     result = first_num * second_num * third_num
+# 
+#     if result < 0:
+#         print('negative')
+#
+#     elif result > 0:
+#         print('positive')
+#
+#     else:
+#         print('zero')
+#
+#
+# first_number = int(input())
+# second_number = int(input())
+# third_number = int(input())
+#
+# check_result(first_number, second_number, third_number)
