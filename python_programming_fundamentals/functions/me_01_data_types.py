d37587307720a9f70f5f42021d80c496906d509f
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-06-27 10:16:51 +0300

    Update me_01_data_types.py

--- python_programming_fundamentals/functions/me_01_data_types.py
+++ python_programming_fundamentals/functions/me_01_data_types.py
@@ -1,23 +1,18 @@
-def data_types(some_text, x):
+def check_data_type(command_data, number):
     result = 0
-    if some_text == "int":
-        result = int(x) * 2
 
-    elif some_text == "real":
-        result = format(float(x) * 1.5, '.2f')
+    if command_data == 'int':
+        result = int(number) * 2
 
-    elif some_text == "string":
-        result = f'${x}$'
+    elif command_data == 'real':
+        result = f"{float(number) * 1.5:.2f}"
+
+    else:
+        result = f'${number}$'
 
     return result
 
 
-text = input()
-other_text = input()
-
-total_result = data_types(text, other_text)
-print(total_result)
-
-
-
-
+data = input()
+num = input()
+print(check_data_type(data, num))
