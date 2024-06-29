445721f3a18d19eb02e048dbb5721429ebfc3765
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-06-29 23:28:35 +0300

    Update l_02_trains.py

--- python_programming_fundamentals/lists_advance/l_02_trains.py
+++ python_programming_fundamentals/lists_advance/l_02_trains.py
@@ -1,21 +1,47 @@
-number_of_wagons = [0 for x in range(int(input()))]
-command = input()
+wagons = int(input())
+command = input().split()
+train = [0] * wagons
 
-while command != 'End':
-    if "add" in command:
-        cmd, people = command.split()
-        people = int(people)
-        number_of_wagons[len(number_of_wagons) - 1] += people
-    elif "insert" in command:
-        cmd, index, people = command.split()
-        index = int(index)
-        people = int(people)
-        number_of_wagons[index] += people
-    elif "leave" in command:
-        cmd, index, people = command.split()
-        index = int(index)
-        people = int(people)
-        number_of_wagons[index] -= people
-    command = input()
+while command[0] != "End":
 
-print(number_of_wagons)
+    if command[0] == 'add':
+        train[-1] += int(command[1])
+
+    elif command[0] == 'insert':
+        index = int(command[1])
+        train[index] += int(command[2])
+
+    elif command[0] == 'leave':
+        index = int(command[1])
+        train[index] -= int(command[2])
+
+    command = input().split()
+
+print(train)
+
+# solution 2
+# number_of_wagons = [0 for x in range(int(input()))]
+# command = input()
+#
+# while command != 'End':
+#
+#     if "add" in command:
+#         action, people = command.split()
+#         people = int(people)
+#         number_of_wagons[len(number_of_wagons) - 1] += people
+#
+#     elif "insert" in command:
+#         action, index, people = command.split()
+#         index = int(index)
+#         people = int(people)
+#         number_of_wagons[index] += people
+#
+#     elif "leave" in command:
+#         action, index, people = command.split()
+#         index = int(index)
+#         people = int(people)
+#         number_of_wagons[index] -= people
+#
+#     command = input()
+#
+# print(number_of_wagons)
