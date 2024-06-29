abd219d5f629380bb380be5d5eab6d755932ef42
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-06-29 21:24:14 +0300

    Update l_01_no_vowels.py

--- python_programming_fundamentals/lists_advance/l_01_no_vowels.py
+++ python_programming_fundamentals/lists_advance/l_01_no_vowels.py
@@ -1,3 +1,6 @@
 text = input()
-result_str = ''.join([char for char in text if char.lower() not in 'aeiou'])
-print(result_str)
+vowels = 'aoueiAOUEI'
+print(*[w for w in text if w not in vowels], sep='')
+
+# solution 2
+# print("".join([char for char in input() if char.lower() not in "'a', 'o', 'u', 'e', 'i'"]))
