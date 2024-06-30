0b0759ae0d23eedc3c07e3ca230f67b2fd0bf505
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-06-30 18:33:00 +0300

    Update l_04_palindrome_strings.py

--- python_programming_fundamentals/lists_advance/l_04_palindrome_strings.py
+++ python_programming_fundamentals/lists_advance/l_04_palindrome_strings.py
@@ -1,4 +1,30 @@
 words = input().split()
-searched_palindromes = input()
-print([word for word in words if word == word[::-1]])
-print(f"Found palindrome {words.count(searched_palindromes)} times")
+palindrome = input()
+found_palindromes = []
+counter = 0
+
+for word in words:
+    if word == word[::-1]:
+        found_palindromes.append(word)
+
+for var in found_palindromes:
+    if var == palindrome:
+        counter += 1
+
+print(found_palindromes)
+print(f"Found palindrome {counter} times")
+
+# solution 2
+# words = input().split()
+# palindrome = input()
+# found_palindromes = []
+#
+# for p in words:
+#     if p in words:
+#         found_palindromes.append(p)
+
+# solution 3
+# words = input().split()
+# searched_palindromes = input()
+# print([word for word in words if word == word[::-1]])
+# print(f"Found palindrome {words.count(searched_palindromes)} times")
