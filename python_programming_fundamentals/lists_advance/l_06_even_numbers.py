2c07aa1c3367c065ed6c9e29306206cc24961a5f
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-06-30 19:57:45 +0300

    Update l_06_even_numbers.py

--- python_programming_fundamentals/lists_advance/l_06_even_numbers.py
+++ python_programming_fundamentals/lists_advance/l_06_even_numbers.py
@@ -1,2 +1,9 @@
-nums = list(map(int, input().split(", ")))
-print([index for index in range(len(nums)) if nums[index] % 2 == 0])
+nums = [int(x) for x in input().split(', ')]
+print([index for index, num in enumerate(nums) if num % 2 == 0])
+
+# double comprehension
+# print([index for index, num in enumerate([int(num) for num in input().split(", ")]) if num % 2 == 0])
+
+# solution 3
+# nums = list(map(int, input().split(", ")))
+# print([index for index in range(len(nums)) if nums[index] % 2 == 0])
