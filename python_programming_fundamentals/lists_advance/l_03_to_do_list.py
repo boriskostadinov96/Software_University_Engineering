90b765b5c3d9618b246df2403205c3963a12861c
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-06-30 18:05:02 +0300

    Update l_03_to_do_list.py

--- python_programming_fundamentals/lists_advance/l_03_to_do_list.py
+++ python_programming_fundamentals/lists_advance/l_03_to_do_list.py
@@ -1,11 +1,12 @@
-command = input()
-notes = [0] * 10
+notes = input()
+to_do_list = [0] * 10
 
-while command != "End":
-    importance, task = command.split('-')
+while notes != "End":
+    importance, task = notes.split('-')
     importance = int(importance)
     index = importance - 1
-    notes[index] = task
-    command = input()
+    to_do_list[index] = task
 
-print([task for task in notes if task != 0])
+    notes = input()
+
+print([t for t in to_do_list if t != 0])
