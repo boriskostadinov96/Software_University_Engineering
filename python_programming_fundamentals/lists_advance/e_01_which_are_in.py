29aeecdde3c368daf6012f82cad7d67f895850e7
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-07-01 18:30:31 +0300

    Update e_01_which_are_in.py

--- python_programming_fundamentals/lists_advance/e_01_which_are_in.py
+++ python_programming_fundamentals/lists_advance/e_01_which_are_in.py
@@ -1,11 +1,16 @@
-first_sequence = input().split(', ')
-second_sequence = input().split(', ')
-substrings = []
+# first_sequence = input().split(', ')
+# second_sequence = input().split(', ')
+# substrings = []
+#
+# for s1 in first_sequence:
+#     for s2 in second_sequence:
+#         if s1 in s2:
+#             substrings.append(s1)
+#             break
+#
+# print(substrings)
 
-for s1 in first_sequence:
-    for s2 in second_sequence:
-        if s1 in s2:
-            substrings.append(s1)
-            break
-
-print(substrings)
+# solution 2
+# first_sequence = input().split(', ')
+# second_sequence = input().split(', ')
+# print([first_str for first_str in first_sequence if any(first_str in second_str for second_str in second_sequence)])
