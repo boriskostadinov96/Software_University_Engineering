7efacccad26e2c69276950d8d52e0ac2f2c007fc
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-07-01 16:48:38 +0300

    Update e_04_number_classification.py

--- python_programming_fundamentals/lists_advance/e_04_number_classification.py
+++ python_programming_fundamentals/lists_advance/e_04_number_classification.py
@@ -1,18 +1,11 @@
-nums = [int(n) for n in input().split(", ")]
-positive_nums = []
-negative_nums = []
-even_nums = []
-odd_nums = []
-for n in nums:
-    if n >= 0:
-        positive_nums.append(n)
-    if n < 0:
-        negative_nums.append(n)
-    if n % 2 == 0:
-        even_nums.append(n)
-    else:
-        odd_nums.append(n)
-print(f'Positive: {", ".join([str(num) for num in positive_nums])}')
-print(f'Negative: {", ".join([str(num) for num in negative_nums])}')
-print(f'Even: {", ".join([str(num) for num in even_nums])}')
-print(f'Odd: {", ".join([str(num) for num in odd_nums])}')
+numbers = [int(x) for x in input().split(', ')]
+
+positive_nums = [x for x in numbers if x >= 0]
+negative_nums = [x for x in numbers if x < 0]
+even_nums = [x for x in numbers if x % 2 == 0]
+odd_nums = [x for x in numbers if x % 2 != 0]
+
+print("Positive:", ', '.join(map(str, positive_nums)))
+print("Negative:", ', '.join(map(str, negative_nums)))
+print("Even:", ', '.join(map(str, even_nums)))
+print("Odd:", ', '.join(map(str, odd_nums)))
