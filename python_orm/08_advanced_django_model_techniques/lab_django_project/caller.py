6953cb0d341eec99f2329e3077e212b1bfb4cde2
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-07-11 11:30:24 +0300

    commit

---
+++ python_orm/08_advanced_django_model_techniques/lab_django_project/caller.py
@@ -0,0 +1,12 @@
+import os
+import django
+from django.core.exceptions import ValidationError
+
+# Set up Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
+django.setup()
+
+# Import your models here
+from main_app.models import Restaurant, Menu
+
+# Create queries within functions
