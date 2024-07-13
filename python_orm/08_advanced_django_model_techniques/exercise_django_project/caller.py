2ef921ae8039f9d3178ef8b541ebb1127c150a00
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-07-13 20:21:40 +0300

    commit

---
+++ python_orm/08_advanced_django_model_techniques/exercise_django_project/caller.py
@@ -0,0 +1,13 @@
+import os
+import django
+
+# Set up Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
+django.setup()
+
+# Import your models here
+from django.core.exceptions import ValidationError
+
+from main_app.models import Book
+
+# Create queries within functions
