970be4ab4cc3d39ccf715e1d1434987e202b1a44
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-08-04 20:19:29 +0300

    Create 01-Sum-First-and-Last-Array-Elements.js

---
+++ JS Front-End/02. JS Arrays & Strings/01-Sum-First-and-Last-Array-Elements.js
@@ -0,0 +1,9 @@
+function solve(numbers) {
+    const firstNumber = numbers[0]
+    const secondNUmber = numbers[numbers.length-1]
+    
+    console.log(firstNumber + secondNUmber);
+         
+}
+
+solve([10, 17, 22, 33])
