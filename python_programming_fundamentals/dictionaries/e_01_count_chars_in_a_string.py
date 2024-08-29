b6c75dcd8511ed74c12575a85feeddef469998f3
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-08-29 10:32:10 +0300

    Delete e_01_count_chars_in_a_string.py

--- python_programming_fundamentals/dictionaries/e_01_count_chars_in_a_string.py
+++
@@ -1,9 +0,0 @@
-symbols = [character for character in input() if character != " "]
-letters = {}
-for symbol in symbols:
-    if symbol not in letters.keys():
-        letters[symbol] = 0
-    letters[symbol] += 1
-
-for symbol, occurrences in letters.items():
-    print(f"{symbol} -> {occurrences}")
