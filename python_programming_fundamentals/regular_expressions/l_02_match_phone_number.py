f938cefde287e6a3db879305c4d347b2aef8a69a
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-09-10 21:43:03 +0300

    Delete l_02_match_phone_number.py

--- python_programming_fundamentals/regular_expressions/l_02_match_phone_number.py
+++
@@ -1,7 +0,0 @@
-import re
-
-pattern = r"\+359 2 \d{3} \d{4}|\+359-2-\d{3}-\d{4}\b"
-text = input()
-
-numbers = re.findall(pattern, text)
-print(*numbers, sep=", ")    # print(", ".join(numbers)) --> same result
