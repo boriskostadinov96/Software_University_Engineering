59e32fc61989ba14b93c261620472d9adc167296
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-09-21 17:05:15 +0300

    Create e-02-Bonus-Score.js

---
+++ JavaScript Basics/02. Conditional Statements/e-02-Bonus-Score.js
@@ -0,0 +1,23 @@
+function solve(initialPoints) {
+    let bonusPoints = 0;
+
+    if (initialPoints <= 100) {
+        bonusPoints += 5;
+    
+    } else if (initialPoints <= 1000) {
+        bonusPoints += initialPoints * 0.2;
+
+    } else {
+        bonusPoints += initialPoints * 0.1;
+    
+    } if (initialPoints % 2 === 0) {
+        bonusPoints += 1;
+    
+    } else if (initialPoints % 10 === 5) {
+        bonusPoints += 2;
+    }
+
+    console.log(bonusPoints);
+    console.log(initialPoints + bonusPoints);
+    
+}   
