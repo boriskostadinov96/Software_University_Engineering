76902a09c792ef44fd8316f731508d1dee9939b5
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-09-21 15:22:27 +0300

    Create l-05-Number-100-200.js

---
+++ JavaScript Basics/02. Conditional Statements/l-05-Number-100-200.js
@@ -0,0 +1,14 @@
+function checkRange(number) {
+    if (number < 100) {
+        console.log("Less than 100");
+        
+    } else if (number > 200) {
+        console.log("Greater than 200");
+        
+    } else {
+        console.log("Between 100 and 200");
+        
+    }
+}
+
+checkRange(230)
