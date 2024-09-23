17880c6b23381229b01c5bcc516a147560e59243
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-09-23 20:29:14 +0300

    Delete l_01_recursive_array_sum.py

--- python_programming_advanced/algorithms/l_01_recursive_array_sum.py
+++
@@ -1,9 +0,0 @@
-def calc_sum(nums, idx=0):
-    if idx == len(nums) - 1:
-        return nums[idx]
-
-    return nums[idx] + calc_sum(nums, idx + 1)
-
-
-nums = [int(x) for x in input().split()]
-print(calc_sum(nums))
