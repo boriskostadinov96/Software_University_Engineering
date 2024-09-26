f8ee294da1e27266342d4bb40cd1655ddfdefe35
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-09-26 19:56:40 +0300

    Update l_04_parking_lot.py

--- python_programming_advanced/tuples_and_sets/l_04_parking_lot.py
+++ python_programming_advanced/tuples_and_sets/l_04_parking_lot.py
@@ -7,7 +7,7 @@ for _ in range(n):
     if direction == "IN":
         parking_lot.add(car_number)
 
-    elif direction == "OUT" and len(parking_lot) != 0:
+    elif direction == "OUT" and car_number in parking_lot:
         parking_lot.remove(car_number)
 
 if parking_lot:
