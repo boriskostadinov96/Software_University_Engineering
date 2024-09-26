77507061a10108a2667639282b1e4a4b19dec5f3
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-09-26 12:30:26 +0300

    Delete l_01_count_same_values.py

--- python_programming_advanced/tuples_and_sets/l_01_count_same_values.py
+++
@@ -1,9 +0,0 @@
-some_numbers = tuple([float(num) for num in input().split()])
-same_values = {}
-
-for current_num in some_numbers:
-    if current_num not in same_values:
-        same_values[current_num] = some_numbers.count(current_num)
-
-for number, count in same_values.items():
-    print(f"{number} - {count} times")
