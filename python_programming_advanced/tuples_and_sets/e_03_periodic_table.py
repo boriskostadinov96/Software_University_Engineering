9b559a1f57bb94472ea2e6b156d8ce2a3f5af891
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-09-26 23:39:24 +0300

    Delete e_03_periodic_table.py

--- python_programming_advanced/tuples_and_sets/e_03_periodic_table.py
+++
@@ -1,8 +0,0 @@
-count_of_input_lines = int(input())
-periodic_table = set()
-
-for _ in range(count_of_input_lines):
-    for chemical in input().split():
-        periodic_table.add(chemical)
-
-print(*periodic_table, sep="\n")
