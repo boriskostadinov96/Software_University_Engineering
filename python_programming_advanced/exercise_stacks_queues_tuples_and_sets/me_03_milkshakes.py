f34d89185311877bff72edf690c8be3a7626c2e3
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-09-28 14:03:45 +0300

    Update me_03_milkshakes.py

--- python_programming_advanced/exercise_stacks_queues_tuples_and_sets/me_03_milkshakes.py
+++ python_programming_advanced/exercise_stacks_queues_tuples_and_sets/me_03_milkshakes.py
@@ -11,9 +11,11 @@ while milkshakes != 5 and chocolates and cups_of_milk:
 
     if chocolate <= 0 and cup_of_milk <= 0:
         continue
+
     elif chocolate <= 0:
         cups_of_milk.appendleft(cup_of_milk)
         continue
+
     elif cup_of_milk <= 0:
         chocolates.append(chocolate)
         continue
