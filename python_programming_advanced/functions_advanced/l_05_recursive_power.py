d691492b9d6f111f2a04011946246092d4ab43c2
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-10-01 12:18:51 +0300

    Create l_05_recursive_power.py

---
+++ python_programming_advanced/functions_advanced/l_05_recursive_power.py
@@ -0,0 +1,17 @@
+def recursive_power(number,power):
+    if power == 1:
+        return number
+
+    return number * recursive_power(number, power - 1)
+
+
+print(recursive_power(2, 10))
+
+# 1.1
+# def recursive_power(number, power):
+#     result = number ** power
+#     return result
+#
+#
+# print(recursive_power(2, 10))
+# print(recursive_power(10, 100))
