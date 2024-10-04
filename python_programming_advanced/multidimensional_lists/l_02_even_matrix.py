bff850d3d917269b512b86c2ffde986b454ab310
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-10-04 13:37:51 +0300

    Delete l_02_even_matrix.py

--- python_programming_advanced/multidimensional_lists/l_02_even_matrix.py
+++
@@ -1,8 +0,0 @@
-rows = int(input())
-matrix = []
-
-for row in range(rows):
-    columns_elements = [int(el) for el in input().split(", ") if int(el) % 2 == 0]
-    matrix.append(columns_elements)
-
-print(matrix)
