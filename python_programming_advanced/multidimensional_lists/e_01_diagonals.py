a3af62c704b34097d8d918b22522774a70943943
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-10-05 19:09:53 +0300

    Delete e_01_diagonals.py

--- python_programming_advanced/multidimensional_lists/e_01_diagonals.py
+++
@@ -1,9 +0,0 @@
-rows = int(input())
-
-square_matrix = [[int(el) for el in input().split(", ")] for _ in range(rows)]
-
-primary = [square_matrix[r][r] for r in range(rows)]
-second = [square_matrix[r][rows - r - 1] for r in range(rows)]
-
-print(f"Primary diagonal: {', '.join(str(x) for x in primary)}. Sum: {sum(primary)}")
-print(f"Secondary diagonal: {', '.join(str(x) for x in second)}. Sum: {sum(second)}")
