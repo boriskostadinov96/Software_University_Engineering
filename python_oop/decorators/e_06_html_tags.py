8d1666c4ea7a383037f2f6ea014129134fa9e3c3
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-11-02 20:16:57 +0200

    Create e_06_html_tags.py

---
+++ python_oop/decorators/e_06_html_tags.py
@@ -0,0 +1,16 @@
+def tags(html_tag):
+    def decorator(function):
+        def wrapper(*args):
+            return f"<{html_tag}>{function(*args)}</{html_tag}>"
+
+        return wrapper
+
+    return decorator
+
+
+@tags('p')
+def join_strings(*args):
+    return "".join(args)
+
+
+print(join_strings("Hello", " you!"))
