854c351de583bf28b2aa9d01e1342ea90d1a8888
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-11-09 20:16:26 +0200

    Create l-06-Number-in-Range.js

---
+++ JavaScript Basics/03. Conditional Statements Advanced/l-06-Number-in-Range.js
@@ -0,0 +1,10 @@
+function checkInterval(number) {
+    if (number !== 0 && number >= -100 && number <= 100) {
+        console.log("Yes");
+    
+    } else {
+        console.log("No");
+    }
+}
+
+checkInterval(-25); 
