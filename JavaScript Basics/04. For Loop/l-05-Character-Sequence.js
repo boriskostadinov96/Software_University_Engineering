324b555b3dac5a4c3cd6dcd0a7863c9e492aa6c4
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-11-18 23:28:42 +0200

    Create l-05-Character-Sequence.js

---
+++ JavaScript Basics/04. For Loop/l-05-Character-Sequence.js
@@ -0,0 +1,9 @@
+function printCharacters(text) {
+    for (let i = 0; i < text.length; i++) {
+        console.log(text[i]);
+    }
+}
+
+printCharacters("softuni");
+printCharacters("ice cream");
+
