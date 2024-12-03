354e14c5d176ec53bd3655d0bd57fc90a8ccda66
boriskostadinov96 <140704393+boriskostadinov96@users.noreply.github.com>
2024-12-04 01:04:36 +0200

    Delete cheetah.py

--- python_oop/encapsulation/e_01_wild_cat_zoo/project/cheetah.py
+++
@@ -1,8 +0,0 @@
-from project.animal import Animal
-
-
-class Cheetah(Animal):
-    MONEY = 60
-
-    def __init__(self, name: str, gender: str, age: int):
-        super().__init__(name, gender, age, self.MONEY)
