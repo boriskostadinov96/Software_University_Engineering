f154a10ee7c18512d556e8f24af2adc16db134cd
boriskostadinov96 <140704393+boriskostadinov96@users.noreply.github.com>
2024-12-04 01:04:53 +0200

    Delete lion.py

--- python_oop/encapsulation/e_01_wild_cat_zoo/project/lion.py
+++
@@ -1,8 +0,0 @@
-from project.animal import Animal
-
-
-class Lion(Animal):
-    MONEY = 50
-
-    def __init__(self, name: str, gender: str, age: int):
-        super().__init__(name, gender, age, self.MONEY)
