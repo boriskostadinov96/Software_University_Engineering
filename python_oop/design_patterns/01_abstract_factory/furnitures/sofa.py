856057dbf287646864fad76ca642a6cf36bf72bf
boriskostadinov96 <140704393+boriskostadinov96@users.noreply.github.com>
2024-12-22 13:45:30 +0200

    Create sofa.py

---
+++ python_oop/design_patterns/01_abstract_factory/furnitures/sofa.py
@@ -0,0 +1,15 @@
+from factories.abstract_factory import AbstractFactory
+
+
+class Sofa(AbstractFactory):
+    def __init__(self, _type):
+        self.type = _type
+        
+    def create_chair(self):
+        pass
+
+    def create_sofa(self):
+        pass
+
+    def create_table(self):
+        pass
