bbb8c0d147df530e7978ea4ce715badcb85e4139
boriskostadinov96 <140704393+boriskostadinov96@users.noreply.github.com>
2025-01-06 22:25:00 +0200

    Create hierarchicalInheritance.ts

---
+++ TypeScript/02. OOP/hierarchicalInheritance.ts
@@ -0,0 +1,25 @@
+class Animal {
+  eat() {
+    console.log("eating...");
+  }
+}
+
+class Dog extends Animal {
+  bark() {
+    console.log("barking...");
+  }
+}
+
+class Cat extends Animal {
+  meow() {
+    console.log("meowing...");
+  }
+}
+
+const animal = new Animal();
+const dog = new Dog();
+const cat = new Cat();
+
+animal.eat();
+dog.bark();
+cat.meow();
