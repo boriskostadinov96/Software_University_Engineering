a81c8b6068d0aa0d4df895c38862327285744fe6
boriskostadinov96 <140704393+boriskostadinov96@users.noreply.github.com>
2025-01-20 21:38:55 +0200

    Create Header.js

---
+++ React/04. Workshop Components/client/src/components/common/Header.js
@@ -0,0 +1,9 @@
+export const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo">
+        <span className="description">User List</span>
+      </div>
+    </header>
+  );
+};
